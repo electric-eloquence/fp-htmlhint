7080e50d22d58eda6f05c346d5f3a784a7db0af1
e2tha-e <ee@electric-eloquence.io>
2016-12-17 09:32:08 -0500

    extensible install.js

--- install.js
+++ install.js
@@ -2,14 +2,34 @@
 
 const fs = require('fs');
 
-const appDir = '../../../app';
-const rcFile = '.htmlhintrc';
+const rootDir = `${process.cwd()}/../../..`;
+const confFile = `${rootDir}/conf.yml`;
+const rcFile = process.argv[2];
 
-if (fs.existsSync(appDir)) {
-  fs.readFile(rcFile, (err, data) => {
+if (fs.existsSync(confFile)) {
+  fs.readFile(confFile, {encoding: 'utf8'}, (err, data) => {
     if (err) {
       throw err;
     }
-    fs.writeFile(`${appDir}/${rcFile}`, data);
+
+    let appDir = `${rootDir}/`;
+    let regex = /^app_dir\s*:.*$/m;
+
+    if (regex.test(data)) {
+      let appDirConf = data.match(regex)[0].trim().split(':');
+      appDir += appDirConf[1].trim();
+    }
+    else {
+      appDir += 'app';
+    }
+
+    if (fs.existsSync(appDir)) {
+      fs.readFile(rcFile, (err1, data1) => {
+        if (err1) {
+          throw err1;
+        }
+        fs.writeFile(`${appDir}/${rcFile}`, data1);
+      });
+    }
   });
 }
