63a43e39439be9bc97cdbfe633463479ea2df5b6
e2tha-e <ee@electric-eloquence.io>
2016-12-18 21:18:19 -0500

    extensible htmlhint~extend.js

--- htmlhint~extend.js
+++ htmlhint~extend.js
@@ -5,10 +5,12 @@ const plugins = require('gulp-load-plugins')();
 
 const appDir = global.appDir;
 const pubDir = global.conf.ui.paths.public;
+const workDir = global.workDir;
+
 const utils = require(`${appDir}/core/lib/utils`);
 
 gulp.task('htmlhint', function () {
   return gulp.src(utils.pathResolve(pubDir.patterns) + '/*/!(index|*markup-only).html')
-    .pipe(plugins.htmlhint('.htmlhintrc'))
+    .pipe(plugins.htmlhint({htmlhintrc: `${workDir}/.htmlhintrc`}))
     .pipe(plugins.htmlhint.reporter());
 });
--- install.js
+++ install.js
@@ -1,35 +1,21 @@
 'use strict';
 
+const findUp = require('find-up');
 const fs = require('fs');
+const path = require('path');
 
-const rootDir = `${process.cwd()}/../../..`;
-const confFile = `${rootDir}/conf.yml`;
+var filepath = findUp.sync('fepper.command');
+
+if (!filepath) {
+  return;
+}
+
+const rootDir = path.dirname(filepath);
 const rcFile = process.argv[2];
 
-if (fs.existsSync(confFile)) {
-  fs.readFile(confFile, {encoding: 'utf8'}, (err, data) => {
-    if (err) {
-      throw err;
-    }
-
-    let appDir = `${rootDir}/`;
-    let regex = /^app_dir\s*:.*$/m;
-
-    if (regex.test(data)) {
-      let appDirConf = data.match(regex)[0].trim().split(':');
-      appDir += appDirConf[1].trim();
-    }
-    else {
-      appDir += 'app';
-    }
-
-    if (fs.existsSync(appDir)) {
-      fs.readFile(rcFile, (err1, data1) => {
-        if (err1) {
-          throw err1;
-        }
-        fs.writeFile(`${appDir}/${rcFile}`, data1);
-      });
-    }
-  });
-}
+fs.readFile(rcFile, (err1, data1) => {
+  if (err1) {
+    throw err1;
+  }
+  fs.writeFile(`${rootDir}/${rcFile}`, data1);
+});
