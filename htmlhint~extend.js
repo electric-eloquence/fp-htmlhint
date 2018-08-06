038686615e38014fc4af7d70bfd1ed39c4f4c71b
e2tha-e <ee@electric-eloquence.io>
2018-08-06 12:59:18 -0400

    dependency updates

--- htmlhint~extend.js
+++ htmlhint~extend.js
@@ -1,16 +1,13 @@
 'use strict';
 
 const gulp = require('gulp');
-const plugins = require('gulp-load-plugins')();
+const htmlhint = require('gulp-htmlhint');
 
-const appDir = global.appDir;
 const pubDir = global.conf.ui.paths.public;
-const workDir = global.workDir;
-
-const utils = require(`${appDir}/core/lib/utils`);
+const rootDir = global.rootDir;
 
 gulp.task('htmlhint', function () {
-  return gulp.src(utils.pathResolve(pubDir.patterns) + '/*/!(index|*markup-only).html')
-    .pipe(plugins.htmlhint({htmlhintrc: `${workDir}/.htmlhintrc`}))
-    .pipe(plugins.htmlhint.reporter());
+  return gulp.src(pubDir.patterns + '/*/!(index|*markup-only).html')
+    .pipe(htmlhint({htmlhintrc: `${rootDir}/.htmlhintrc`}))
+    .pipe(htmlhint.reporter());
 });
--- install.js
+++ install.js
@@ -1,21 +1,23 @@
 'use strict';
 
-const findUp = require('find-up');
 const fs = require('fs');
 const path = require('path');
 
-var filepath = findUp.sync('fepper.command');
+const findUp = require('find-up');
+
+const filepath = findUp.sync('fepper.command');
 
 if (!filepath) {
   return;
 }
 
-const rootDir = path.dirname(filepath);
 const rcFile = process.argv[2];
+const rootDir = path.dirname(filepath);
 
-fs.readFile(rcFile, (err1, data1) => {
-  if (err1) {
-    throw err1;
+fs.readFile(rcFile, (err, data) => {
+  if (err) {
+    throw err;
   }
-  fs.writeFile(`${rootDir}/${rcFile}`, data1);
+
+  fs.writeFile(`${rootDir}/${rcFile}`, data);
 });
